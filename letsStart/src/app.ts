ae4a573af0852ff3cf32d5d081f220dfb53f9c43
euijinkk <gytks4@naver.com>
2023-02-18 14:11:20 +0900

    second

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,13 +1,12 @@
+import { Cat } from "./app.model";
 import * as express from "express";
 
 const app: express.Express = express(); // express instance
-const port = 8000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  console.log("req", req);
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send({ data: Cat });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(8000, () => {
+  console.log("server is on...");
 });
